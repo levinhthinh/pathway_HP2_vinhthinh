53451dcc22964b6415874c4f1b2a61d2f161c8fb
levinhthinh <143257011+levinhthinh@users.noreply.github.com>
2024-08-23 15:59:08 +0700

    ghi chu

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ giohang_chitiet=([1,'AL',3,200000],[1,'ASM',2,25000])
 # hanghoa=(['id_hang','ten', 'gia tien', 'hang ton'])
 hanghoa = (['AL','áo len',200000,10],['ASM','áo sơmi',25000,9],['A','áo a',15000,2],['B','áo b',10000,0],['C','áo c',500,6])
 
-
+# menu = ([ten chuc nang, quyen duoc xem])
 menu=(['kiem tra gio hang',[1,2]])
 run=True
 while run:
--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 # hangHoa = ['a', 'b', 'c', 'd', 'e']
 # 1: a
 # 2: b ....
-def getDataArray():
-    return None
+def getDataArray(arr):
+    

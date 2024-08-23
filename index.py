8a2efe2a26889cb37817b4fe4b172139159acf08
levinhthinh <143257011+levinhthinh@users.noreply.github.com>
2024-08-23 15:40:33 +0700

    khai bao bien
    cua hang online

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
-print('hello world')
-print('world')
+# user([id],ten,mk,id dac biet(1=super user))
+user=([1,'tuan','abc',1])
+
+# giohang=([id_user, id_giohang])
+giohang=([1,1])
+
+# giohang_chitiet=([id_giohang, id_hang, so luong, gia tien])
+giohang_chitiet=([1,'AL',3,200000],[1,'ASM',2,25000])
+
+# hanghoa=(['id_hang','ten', 'gia tien', 'hang ton'])
+hanghoa = (['AL','áo len',200000,10],['ASM','áo sơmi',25000,9],['A','áo a',15000,2],['B','áo b',10000,0],['C','áo c',500,6])
